011909653d28410a51d4f80da32c32bf272f166a
scastellote <scastellote@opentrends.net>
2018-05-19 00:09:42 +0200

    añadir images a assets para poder visualizar las imagenes

--- angular5-my-proyect/src/app/in-memory-data.service.ts
+++ angular5-my-proyect/src/app/in-memory-data.service.ts
@@ -3,8 +3,8 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let cyclist = [
-      { id: 1, name: 'Alberto', surname: 'Contador' , img: './alberto-contador-retirada.jpg'},
-      { id: 2, name: 'Marco', surname: 'Pantani', img: {}}
+      { id: 1, name: 'Alberto', surname: 'Contador' , img: 'assets/images/alberto-contador.jpg'},
+      { id: 2, name: 'Marco', surname: 'Pantani', img: 'assets/images/marco-pantani.jpg'}
     ];
     return {cyclist};
   }
